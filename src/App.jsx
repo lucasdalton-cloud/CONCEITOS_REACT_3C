05f662992b37fc95f76cef73f928e219933d1894
LUCAS PATRICK BRUSTOLIN DALTON <lucas_dalton@estudante.sesisenai.org.br>
2026-04-27 11:35:23 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [carro, setCarro] = useState('BMW')
+
+  function trocarDeCarro() {
+    setCarro("Mustang")
+  }
+
+  return (
+    <>
+      <h1>{carro}</h1>
+      <button onClick={trocarDeCarro}>trocar de carro</button>
+    </>
+  )
+}
+
+export default App
