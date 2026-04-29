d4a7a6c785ad3eac7922564d6075254d818f41e9
LUCAS PATRICK BRUSTOLIN DALTON <lucas_dalton@estudante.sesisenai.org.br>
2026-04-29 09:32:34 -0300

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,38 @@
 import { useState } from 'react'
 import './App.css'
+import Login from './Login'
+import Cadastro from './Cadastro'
+import Usuarios from './Usuarios'
 
 function App() {
-  const [carro, setCarro] = useState('BMW')
+  const [tela, setTela] = useState('Login')
 
-  function trocarDeCarro() {
-    setCarro("Mustang")
+  const trocarDeTela = (pagina) => {
+    setTela(pagina)
+  }
+
+  const renderizar = () => {
+    if(tela === 'login'){
+      return <Login/>
+    } else if(tela === 'cadastro') {
+      return <Cadastro/>
+    } else if(tela === 'usuarios') {
+      return <Usuarios/>
+    } else{
+      return <Login/>
+    }
   }
 
   return (
     <>
-      <h1>{carro}</h1>
-      <button onClick={trocarDeCarro}>trocar de carro</button>
+      <button onClick={() => trocarDeTela('cadastro')}>Cadastro</button>
+      <button onClick={() => trocarDeTela('login')}>Login</button>
+      <button onClick={() => trocarDeTela('usuarios')}>Usuarios</button>
+      <hr />
+      {renderizar()}
     </>
   )
+
 }
 
 export default App
