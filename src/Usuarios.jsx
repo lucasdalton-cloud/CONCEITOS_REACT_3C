5cdfc4744d28d47aca2529e6b8acf819e5a3e42e
LUCAS PATRICK BRUSTOLIN DALTON <lucas_dalton@estudante.sesisenai.org.br>
2026-05-08 09:34:55 -0300

    adicionado cont

--- src/Usuarios.jsx
+++ src/Usuarios.jsx
@@ -1,6 +1,30 @@
+import { useEffect, useState } from "react"
 export default function Usuarios(){
-    
+    const [cont, setCont] = useState(0)
+
+    const aumenta = () => {
+        setCont(cont + 1)
+    }
+
+    const dimuir = () => {
+        setCont(cont - 1)
+    }
+
+    useEffect(() => {
+        document.title = `count: ${cont}`
+    }, [cont])
+
+  useEffect(() => {
+    if (cont > 10  )alert ("contador passou de 10")
+    if (cont === -10)alert ("contador chegou a -10")
+    }, [cont])
+
     return (
-        <h1>USUARIOS</h1>
+        <div>
+            <h1>USUARIOS</h1>
+            {cont}
+            <button onClick={() => aumenta()}>aumenta</button>
+            <button onClick={() => dimuir()}>dimuir</button>
+        </div>
     )
 }
